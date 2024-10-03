e071ad9fc473795edcf2f257b479bf65a2ff5cb0
Cesar Bretana <bretanac@gmail.com>
2024-10-03 17:44:17 +0200

    feat: add neotree

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,7 +28,16 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.8', 
     dependencies = { 'nvim-lua/plenary.nvim' } 
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  }
 }
 local opts = {}
 
@@ -37,6 +46,7 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem toggle<CR>', {})
 
 local config = require("nvim-treesitter.configs")
 config.setup({
