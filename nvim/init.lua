3818620738ffca3c224eb8cd54a5a1473a9d5368
Cesar Bretana <bretanac@gmail.com>
2024-10-03 12:41:51 +0200

    feat: add lazy-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
+vim.g.mapleader = " "
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -26,13 +27,23 @@ local plugins = {
   { 
     'nvim-telescope/telescope.nvim', tag = '0.1.8', 
     dependencies = { 'nvim-lua/plenary.nvim' } 
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
+
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = { "lua", "go", "php", "javascript", "html" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
 
 vim.cmd.colorscheme "nord"
 
