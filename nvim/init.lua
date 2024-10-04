e6f3455ecbf9cdc4fe25fdfdcad4cb2afd886621
Cesar Bretana <bretanac@gmail.com>
2024-10-04 09:39:09 +0200

    refactor: encapsulate plugins configurations in their own dirs

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,17 +23,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-local builtin = require("telescope.builtin")
-
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>n', ':Neotree filesystem toggle<CR>', {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = { "lua", "go", "php", "javascript", "html" },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
 
 
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -5,5 +5,8 @@
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
-    }
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>n', ':Neotree filesystem toggle<CR>', {})
+    end
   }
