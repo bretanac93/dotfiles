24eeed8ee1e4fad193ecee51a293b6d97cbd01d6
Cesar Bretana <bretanac@gmail.com>
2024-10-06 18:07:01 +0200

    feat: enable inline blame

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,8 +1,11 @@
 return {
 	{
 		"lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
 	},
 }
