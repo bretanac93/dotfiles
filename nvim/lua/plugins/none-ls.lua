1c37be8354992593ddb7f95f68480527a793e9f2
Cesar Bretana <bretanac@gmail.com>
2024-10-06 12:59:43 +0200

    feat: automatically install none-ls requirements

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,11 +6,21 @@ return {
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
 					null_ls.builtins.diagnostics.golangci_lint,
 				},
 			})
 
 			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
 		end,
 	},
 }
