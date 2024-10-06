cf48135f63af45091d113c69e76445311cb2cebc
Cesar Bretana <bretanac@gmail.com>
2024-10-06 12:22:25 +0200

    feat: enable gopls

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -17,7 +17,10 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require('lspconfig')
+
       lspconfig.lua_ls.setup({})
+      lspconfig.gopls.setup({})
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
