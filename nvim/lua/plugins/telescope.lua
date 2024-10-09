e03fa6c29049eab9357323b5196c73a0bd6acfdc
Cesar Bretana <cesar.bretana@sumup.com>
2024-10-09 15:04:44 +0200

    feat: add shortcut for telescoping over open buffers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
 
 			vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 		end,
 	},
 	{
