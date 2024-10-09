91f467be02ef42c4a6185cfca29bf68fce1a1148
Cesar Bretana <cesar.bretana@sumup.com>
2024-10-09 15:20:30 +0200

    feat: add shortcut to rename reference

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -31,7 +31,8 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gfr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, {})
     end,
   },
 }
