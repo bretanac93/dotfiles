49cdce2c06181cb4a51874e038e076d151d745bd
Cesar Bretana <cesar.bretana@sumup.com>
2024-10-09 14:44:21 +0200

    refactor: format neo-tree file

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,27 +1,27 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				filtered_items = {
-					visible = true,
-					show_hidden_count = true,
-					hide_dotfiles = false,
-					hide_gitignored = true,
-					hide_by_name = {
-						".git",
-						"node_modules",
-						".DS_Store",
-					},
-				},
-			},
-		})
-		vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            "node_modules",
+            ".DS_Store",
+          },
+        },
+      },
+    })
+    vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle<CR>", {})
+  end,
 }
