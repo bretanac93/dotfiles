844cdc1768ff48941b66476f8f2c8ca4dd30550e
Cesar Bretana <cesar.bretana@sumup.com>
2024-10-11 13:50:25 +0200

    feat: change to nordic

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -6,9 +6,4 @@ set.tabstop = 2
 set.softtabstop = 2
 set.relativenumber = true
 
---vim.cmd("set expandtab")
---vim.cmd("set tabstop=2")
---vim.cmd("set softtabstop=2")
---vim.cmd("set shiftwidth=2")
---vim.cmd("set relativenumber")
 vim.g.mapleader = " "
