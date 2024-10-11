2917ff893c1c3a58f9e9c2afc692b8f4c5a1d521
Cesar Bretana <cesar.bretana@sumup.com>
2024-10-11 10:35:09 +0200

    feat: enable typescript lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -28,6 +28,10 @@ return {
         capabilities = capabilities
       })
 
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
