2d9bec277209ff349c28245c1a1818820f662ff0
Cesar Bretana <bretanac@gmail.com>
2024-10-17 19:02:44 +0200

    feat: customize kitty <> tmux <> nvim integration

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -10,3 +10,12 @@ set.undofile = true
 set.wildmode = "longest:full,full"
 set.updatetime = 100
 set.redrawtime = 10000
+
+vim.cmd [[
+augroup kitty_mp
+    autocmd!
+    au VimLeave * :silent !kitty @ set-spacing padding=20 margin=10
+    au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0
+augroup END
+]]
+
