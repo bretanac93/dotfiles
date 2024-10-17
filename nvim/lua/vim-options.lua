6d7c2dcab3a9fbd7b164317a207e03964222e73d
Cesar Bretana <cesar.bretana@sumup.com>
2024-10-17 13:32:26 +0200

    feat: use system clipboard over vim's

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 local set = vim.opt
 
 set.expandtab = true
@@ -5,5 +7,9 @@ set.shiftwidth = 2
 set.tabstop = 2
 set.softtabstop = 2
 set.relativenumber = true
+set.clipboard = "unnamedplus"
+set.undofile = true
+set.wildmode = "longest:full,full"
+set.updatetime = 100
+set.redrawtime = 10000
 
-vim.g.mapleader = " "
