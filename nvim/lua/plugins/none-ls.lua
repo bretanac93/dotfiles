b30f942bcb022b6bd1101a926cdce6674f48791a
Cesar Bretana <cesar.bretana@sumup.com>
2025-02-06 09:48:43 +0100

    feat: update null-ls

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,8 +6,9 @@ return {
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.prettier,
 					null_ls.builtins.diagnostics.golangci_lint,
+					null_ls.builtins.formatting.black,
 				},
 			})
 
