8cf635a135cd2e357d252744c197380b75d515e9
Cesar Bretana <cesar.bretana@sumup.com>
2025-04-08 11:36:48 +0200

    refactor: dry keymaps definition

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,40 +2,36 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- clear search highlights
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+-- Helper for common keymap opts
+local map = function(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
 
--- Close all open buffers
-vim.keymap.set("n", "<leader>Q", ":bufdo bdelete<CR>")
+-- Normal mode mappings
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+map("n", "<leader>Q", ":bufdo bdelete<CR>")
+map("n", "[b", ":bprev<CR>")
+map("n", "]b", ":bnext<CR>")
+map("n", "<C-J>", ":m .+1<CR>==")
+map("n", "<C-K>", ":m .-2<CR>==")
 
--- Reselect visual block after indent/outdent
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
+-- Visual mode mappings
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+map("v", "y", "myy`y")
+map("v", "Y", "myY`y")
+map("v", "p", '"_dP')
+map("v", "<C-J>", ":m '>+1<CR>gv=gv")
+map("v", "<C-K>", ":m '<-2<CR>gv=gv")
 
--- Maintain the cursor position when yanking a visual selection.
-vim.keymap.set("v", "y", "myy`y")
-vim.keymap.set("v", "Y", "myY`y")
+-- Insert mode mappings
+map("i", ";;", "<Esc>A;<Esc>")
+map("i", ",,", "<Esc>A,<Esc>")
 
--- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
-
--- Paste replace visual selection without copying it.
-vim.keymap.set("v", "p", '"_dP')
-
--- Easy insertion of a trailing ; or , from insert mode.
-vim.keymap.set("i", ";;", "<Esc>A;<Esc>")
-vim.keymap.set("i", ",,", "<Esc>A,<Esc>")
-
-vim.keymap.set("n", "[b", ":bprev<CR>")
-vim.keymap.set("n", "]b", ":bnext<CR>")
-
--- Move current line down
-vim.keymap.set("n", "<C-J>", ":m .+1<CR>==", { noremap = true, silent = true })
--- Move current line up
-vim.keymap.set("n", "<C-K>", ":m .-2<CR>==", { noremap = true, silent = true })
-
--- Move selected lines down
-vim.keymap.set("v", "<C-J>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
--- Move selected lines up
-vim.keymap.set("v", "<C-K>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+-- Expr-based movement for wrapped lines
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
