48cc4c2f38059d96d1e898e81d94d59ce29415e2
Cesar Bretana <bretanac@gmail.com>
2025-04-10 20:14:50 +0200

    chore: override copilot tab behavior

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,12 @@
 return {
 	{
 		"github/copilot.vim",
+		config = function()
+			vim.cmd([[
+				imap <silent><script><expr> <C-c> copilot#Accept("\<CR>")
+				let g:copilot_no_tab_map = v:true
+			]])
+		end,
 	},
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
