8775837c4552f6cad316917222192abb54696a3e
Cesar Bretana <cesar.bretana@sumup.com>
2025-04-10 16:42:14 +0200

    set some QoL vim options

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -5,17 +5,21 @@ set.shiftwidth = 2
 set.tabstop = 2
 set.softtabstop = 2
 set.relativenumber = true
+set.signcolumn = "yes:2"
 set.clipboard = "unnamedplus"
 set.undofile = true
 set.wildmode = "longest:full,full"
+set.completeopt = "menuone,longest,preview"
 set.updatetime = 100
 set.redrawtime = 10000
 
-vim.cmd [[
+set.list = true
+set.listchars = { tab = "▸ ", trail = "•" }
+
+vim.cmd([[
 augroup kitty_mp
     autocmd!
     au VimLeave * :silent !kitty @ set-spacing padding=20 margin=10
     au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0
 augroup END
-]]
-
+]])
