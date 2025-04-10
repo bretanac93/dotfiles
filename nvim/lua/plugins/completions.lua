7316ac96ca70797063b6902ddb2ec05fc10e5474
Cesar Bretana <cesar.bretana@sumup.com>
2025-04-10 16:30:04 +0200

    feat: customize completion engine

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -11,15 +11,27 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+		},
 		config = function()
 			-- Set up nvim-cmp.
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
 			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local has_words_before = function()
+				local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -32,10 +44,31 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
+					{ name = "path" },
 				}, {
 					{ name = "buffer" },
 				}),
