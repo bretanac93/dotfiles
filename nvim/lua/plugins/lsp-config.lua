24b61b601a602dd4132cdff5b4c82d4aefdd257a
Cesar Bretana <cesar.bretana@sumup.com>
2025-04-10 16:14:22 +0200

    feat: add lsp source for diagnostics

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,60 +1,66 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.pyright.setup({
-        capabilities = capabilities
-      })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.kotlin_language_server.setup({
-        capabilities = capabilities
-      })
+			lspconfig.kotlin_language_server.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, {})
-      vim.keymap.set("n", "gd", ":Telescope lsp_definitions<CR>", {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "gi", ':Telescope lsp_implementations<CR>', {})
-      vim.keymap.set("n", "gr", ':Telescope lsp_references<CR>', {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, {})
+			vim.keymap.set("n", "gd", ":Telescope lsp_definitions<CR>", {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gi", ":Telescope lsp_implementations<CR>", {})
+			vim.keymap.set("n", "gr", ":Telescope lsp_references<CR>", {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
 
-      -- Sign configuration
-      vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-      vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
-      vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-      vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
-    end,
-  },
+			-- Sign configuration
+			vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+			vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+			vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+			vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
+			vim.diagnostic.config({
+				float = {
+					source = "if_many",
+				},
+			})
+		end,
+	},
 }
