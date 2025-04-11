4f5e104a18b874a4455d46cb98534274bc398d33
Cesar Bretana <cesar.bretana@sumup.com>
2025-04-11 09:42:09 +0200

    feat: configure copilot chat

--- nvim/lua/user/plugins/copilot.lua
+++ nvim/lua/user/plugins/copilot.lua
@@ -1,15 +1,14 @@
 return {
 	{
 		"github/copilot.vim",
-		config = function()
-			vim.cmd([[
-				imap <silent><script><expr> <C-c> copilot#Accept("\<CR>")
-				let g:copilot_no_tab_map = v:true
-			]])
-		end,
 	},
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		build = "make tiktoken",
+		config = function()
+			require("CopilotChat").setup({
+				model = "claude-3.7-sonnet",
+			})
+		end,
 	},
 }
