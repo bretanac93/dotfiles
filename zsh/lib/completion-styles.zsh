5eaa939eabf463bbe405625dce6745a5eb0039e9
Cesar Bretana <bretanac@gmail.com>
2026-03-14 13:40:24 +0100

    tune: reduce zsh completion noise
    Keep the nicer completion behavior while removing the extra category banners and verbose labels that made the menu feel too busy.

--- zsh/lib/completion-styles.zsh
+++ zsh/lib/completion-styles.zsh
@@ -3,6 +3,7 @@ zmodload zsh/complist 2>/dev/null || true
 zstyle ':completion:*' use-cache true
 zstyle ':completion:*' cache-path "${XDG_CACHE_HOME:-$HOME/.cache}/zsh/completions"
 zstyle ':completion:*' menu select
+zstyle ':completion:*' verbose no
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' squeeze-slashes true
 zstyle ':completion:*' special-dirs true
@@ -10,10 +11,7 @@ zstyle ':completion:*' matcher-list \
   'm:{a-z}={A-Z}' \
   'r:|[._-]=* r:|=*'
 
-zstyle ':completion:*:descriptions' format '[%d]'
-zstyle ':completion:*:messages' format '%d'
-zstyle ':completion:*:warnings' format 'no matches for: %d'
-zstyle ':completion:*:corrections' format '%d (errors: %e)'
+zstyle ':completion:*:warnings' format 'no matches'
 
 if [[ -n "${LS_COLORS:-}" ]]; then
   zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
