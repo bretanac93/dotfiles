ec2221496bc334a224c1dc3d4a57d75720cfb331
Cesar Bretana <bretanac@gmail.com>
2026-03-14 14:18:15 +0100

    refactor: treat personal aliases as the default
    Load tracked aliases from the personal alias set by default so repo-managed shortcuts represent the personal baseline while local alias files keep layering on top for work-specific commands.

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -11,7 +11,7 @@ load_zsh_aliases() {
   local aliases_dir local_aliases_dir
 
   aliases_dir="$ZSH_CONFIG_DIR/aliases"
-  load_zsh_alias_dir "$aliases_dir/common"
+  load_zsh_alias_dir "$aliases_dir/personal"
 
   if [[ "${ZSH_LOAD_LOCAL_CONFIG:-1}" != "0" ]]; then
     local_aliases_dir="${ZSH_LOCAL_CONFIG_DIR:-$HOME/.config/zsh.local}/aliases"
