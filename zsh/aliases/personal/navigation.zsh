fdcb8afc375e3cbfd43ac818425ead2493cdc847
Cesar Bretana <bretanac@gmail.com>
2026-03-15 09:32:37 +0100

    feat: add common git and navigation aliases
    Port the most useful OMZ git shortcuts plus directory navigation conveniences so the native zsh setup feels productive out of the box.

---
+++ zsh/aliases/personal/navigation.zsh
@@ -0,0 +1,29 @@
+if (( $+commands[lsd] )); then
+  alias ls='lsd'
+  alias l='ls -l'
+  alias la='ls -a'
+  alias ll='ls -la'
+  alias lt='ls --tree'
+  alias lla='ls -lA'
+  alias l.='ls -d .*'
+elif (( $+commands[exa] )); then
+  alias ls='exa'
+  alias l='exa -l'
+  alias la='exa -a'
+  alias ll='exa -la'
+  alias lt='exa --tree'
+  alias lla='exa -la'
+  alias l.='exa -d .*'
+else
+  alias l='ls -lFh'
+  alias la='ls -lah'
+  alias ll='ls -lAFh'
+  alias l.='ls -d .*'
+fi
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias -- -='cd -'
