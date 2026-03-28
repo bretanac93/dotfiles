e280f4f204020f654835b9f167615fb8393014f6
Cesar Bretana <bretanac@gmail.com>
2026-03-28 15:06:11 +0100

    fix: lazy.nvim fails to install silently on arch

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -1,14 +1,35 @@
+local uv = vim.uv or vim.loop
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
+local function clone_lazy(extra_args)
+  local clone_cmd = {
     "git",
     "clone",
-    "--filter=blob:none",
     "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git",
+  }
+
+  vim.list_extend(clone_cmd, extra_args)
+  vim.list_extend(clone_cmd, {
+    lazyrepo,
     lazypath,
   })
+
+  vim.fn.system(clone_cmd)
+
+  return vim.v.shell_error == 0
+end
+
+if not uv.fs_stat(lazypath .. "/lua/lazy/init.lua") then
+  vim.fn.mkdir(vim.fs.dirname(lazypath), "p")
+
+  if not clone_lazy({ "--filter=blob:none" }) then
+    vim.fn.delete(lazypath, "rf")
+
+    if not clone_lazy({}) then
+      error("failed to clone lazy.nvim from " .. lazyrepo)
+    end
+  end
 end
 
 vim.opt.rtp:prepend(lazypath)
