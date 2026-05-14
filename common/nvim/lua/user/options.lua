27c049b4eb13ed7d30d8d0ba7f1a62ede5d6048d
Cesar Bretana <bretanac@gmail.com>
2026-05-14 12:32:57 +0200

    fix: remove Kitty terminal padding hooks, Ghostty is the primary terminal

--- common/nvim/lua/user/options.lua
+++ common/nvim/lua/user/options.lua
@@ -14,11 +14,3 @@ set.completeopt = "menuone,longest,preview"
 set.lazyredraw = true
 set.updatetime = 250
 set.redrawtime = 10000
-
-vim.cmd([[
-augroup kitty_mp
-    autocmd!
-    au VimLeave * :silent !kitty @ set-spacing padding=20 margin=10
-    au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0
-augroup END
-]])
